f2cd085bfc54d9d556e4c719d9c390efb73c1fda
Safy555 <112870391+Safy555@users.noreply.github.com>
2024-04-21 22:15:48 +0200

    Add files via upload

--- hra_10.5.2024/graphwar.pas
+++ hra_10.5.2024/graphwar.pas
@@ -7,11 +7,41 @@ type Thrac=record
   end;
 var
   gd,gm:smallint;
-  i,j,stavhry:integer;
-  input,la:string;
+  i,j,stavhry,esc:integer;
+  input,input_old:string;
   ch:char;
   hraci: array [1..2] of Thrac;
-procedure panacik(panacikx,panaciky:integer;strana:string);
+function StringToReal(s:string):real;
+  var
+    code:integer;
+    value:real;
+  begin
+    Val(s,value,code);
+    if code=0 then
+      StringToReal:=value
+    else
+      StringToReal:= 0.0; // Return 0.0 if conversion fails
+  end;
+procedure box(stred_x,stred_y,sirka,vyska,border:integer; farba_stred,farba_vlavo,farba_vpravo:word);    //box s tienmi
+begin
+  setfillstyle(1,farba_stred);
+  bar(stred_x-(sirka div 2)+border,stred_y-(vyska div 2)+border,stred_x+(sirka div 2)-border,stred_y+(vyska div 2)-border);
+  setfillstyle(1,farba_vlavo);
+  bar(stred_x-(sirka div 2),stred_y-(vyska div 2),stred_x+(sirka div 2),stred_y-(vyska div 2)+border);
+  bar(stred_x-(sirka div 2),stred_y-(vyska div 2),stred_x-(sirka div 2)+border,stred_y+(vyska div 2));
+  setfillstyle(1,farba_vpravo);setcolor(farba_vpravo);
+  bar(stred_x+(sirka div 2),stred_y+(vyska div 2),stred_x-(sirka div 2)+border,stred_y+(vyska div 2)-border);
+  bar(stred_x+(sirka div 2),stred_y+(vyska div 2),stred_x+(sirka div 2)-border,stred_y-(vyska div 2)+border);
+  line(stred_x-(sirka div 2),stred_y+(vyska div 2),stred_x-(sirka div 2)+border,stred_y+(vyska div 2));
+  line(stred_x-(sirka div 2),stred_y+(vyska div 2),stred_x-(sirka div 2)+border,stred_y+(vyska div 2)-border);
+  line(stred_x+(sirka div 2),stred_y-(vyska div 2),stred_x+(sirka div 2)-border,stred_y-(vyska div 2)+border);
+  line(stred_x+(sirka div 2),stred_y-(vyska div 2),stred_x+(sirka div 2),stred_y-(vyska div 2)+border);
+  floodfill(stred_x-(sirka div 2)+border-2,stred_y+(vyska div 2)-(border div 2),farba_vpravo);
+  floodfill(stred_x+(sirka div 2)-(border div 2),stred_y-(vyska div 2)+border-2,farba_vpravo);
+
+end;
+
+procedure panacik(panacikx,panaciky:integer;strana:string);  //vypis hracov
 begin
    setfillstyle(1,red);setcolor(red);
    FillEllipse(panacikx,panaciky,hracr,hracr);
@@ -29,7 +59,7 @@ begin
    end;
 end;
 
-procedure generovanie(level:integer);
+procedure generovanie(level:integer);   //generovanie prekazok,hracov
 var
   randx,randy,randr,rand,pocc,rx,ry:integer;
 begin
@@ -41,10 +71,10 @@ begin
   end;
 
   hraci[1].x:=100+hracr+random(((getmaxx-200)div 2)-(2*hracr));
-  hraci[1].y:=100+hracr+random(getmaxy-300-(2*hracr));
+  hraci[1].y:=110+hracr+random(getmaxy-300-(3*hracr));
   panacik(hraci[1].x,hraci[1].y,'p');                    //generovanie panacika vlavo
   hraci[2].x:=(getmaxx div 2)+hracr+random(((getmaxx-200) div 2)-(2*hracr));
-  hraci[2].y:=100+hracr+random(getmaxy-300-(2*hracr));
+  hraci[2].y:=110+hracr+random(getmaxy-300-(3*hracr));
   panacik(hraci[2].x,hraci[2].y,'l');                    //generovanie panacika vpravo
 
   setfillstyle(1,green);setcolor(green);
@@ -63,7 +93,7 @@ begin
              else if hraci[j].y<randy then ry:=randy-hraci[j].y
              else ry:=1;
 
-             if round(sqrt(rx*rx+ry*ry))<randr+hracr+1 then pocc:=pocc+1;
+             if round(sqrt(rx*rx+ry*ry))<randr+hracr+21 then pocc:=pocc+1;
          end;
       until pocc=0;
       FillEllipse(randx,randy,randr,randr);
@@ -74,10 +104,10 @@ begin
 
 end;
 
-function fx(input1:string; c:integer):integer;
+function fx(input1:string; c:integer):integer;   //vypocet funkcnej hodnoty
 var
-  i,j,poc,poc1,kod,cislo,posz:integer;
-  x,res:real;
+  i,j,poc,poc1,kod,posz:integer;
+  x,res,cislo:real;
   s1,s2,znamienko:string;
 begin
   x:=c/k;
@@ -102,28 +132,28 @@ begin
           else  s1:=input1;
           if znamienko='+' then begin
                if s1<>'x' then begin
-                 val(s1,cislo,kod);
+                 cislo:=StringToReal(s1);
                  res:=res+cislo;
                end
                else res:=res+x;
           end
           else if znamienko='-' then begin
                if s1<>'x' then begin
-                 val(s1,cislo,kod);
+                 cislo:=StringToReal(s1);
                  res:=res-cislo;
                end
                else res:=res-x;
           end
           else if znamienko='*' then begin
                if s1<>'x' then begin
-                 val(s1,cislo,kod);
+                 cislo:=StringToReal(s1);
                  res:=res*cislo;
                end
                else res:=res*x;
           end
           else if znamienko='/' then begin
                if s1<>'x' then begin
-                 val(s1,cislo,kod);
+                 cislo:=StringToReal(s1);
                  res:=res/cislo;
                end
                else res:=res/x;
@@ -134,25 +164,51 @@ begin
   end;
 end;
 
-procedure vykreslenie(hracx,hracy:integer);
+procedure vykreslenie(hracx,hracy:integer; vykreslenie_farba:word; inpu:string);  //vykreslenie funckie
 var
-  j,p:integer;
+  j,p,poc:integer;
 begin
-   p:=hraci[1].y-fx(input,hracx-100-((getmaxx-200) div 2));
-   for j:=hracx-100-((getmaxx-200) div 2) to ((getmaxx-200) div 2) do begin
-       if (fx(input,j)+p<getmaxy-200) and (fx(input,j)+p>100) then begin
-            putpixel(j+(getmaxx div 2),fx(input,j)+p,5);
-       end;
-   end;
+   poc:=0;
+   if stavhry=2 then stavhry:=0;
+   p:=hraci[1].y-fx(inpu,hracx-100-((getmaxx-200) div 2));
+   j:=hracx-100-((getmaxx-200) div 2)-1;
+   repeat
+      poc:=poc+1;
+      j:=j+1;
+      if (getpixel(j+(getmaxx div 2),fx(inpu,j)+p)=green) then begin
+           if  vykreslenie_farba<>black then begin
+                setcolor(black);setfillstyle(1,black);
+                FillEllipse(j+(getmaxx div 2),fx(inpu,j)+p,20,20);
+                stavhry:=2;
+           end
+           else  if vykreslenie_farba=black then stavhry:=2;
+
+      end
+      else if (getpixel(j+(getmaxx div 2),fx(inpu,j)+p)=red) and ((hracx+poc)-(hracx)>(hracr+5)) then begin
+           stavhry:=1;
+           setcolor(darkgray);settextstyle(0,0,10);
+           outtextxy((getmaxx div 2)-337,55,'Vyhral si');
+           setcolor(blue);
+           outtextxy((getmaxx div 2)-332,50,'Vyhral si');
+           repeat until keypressed;
+      end
+      else begin
+         putpixel(j+(getmaxx div 2),fx(inpu,j)+p,vykreslenie_farba);
+         //delay(1);
+      end;
+   until (j=((getmaxx-200) div 2)) or ((fx(inpu,j)+p>getmaxy-200)) or ((fx(inpu,j)+p<100)) or (stavhry<>0);
+
+
 end;
 
-procedure pole();          //pozadie
+procedure pole();    //pozadie
 var
   cislox,cisloy:integer;
   cx,cy:string;
 begin
+  cleardevice();
   rectangle(100,100,getmaxx-100,getmaxy-200);
-  moveto((getmaxx div 2)-70,(getmaxy-100) div 2);setcolor(white);
+  moveto((getmaxx div 2)-70,(getmaxy-100) div 2);setcolor(white); settextstyle(0,0,1);
   outtext('loading...');
 
   //pozadie
@@ -249,63 +305,277 @@ begin
   moveto(getmaxx-98,((getmaxy-100)div 2)-4); outtext(cx);
   cx:='-'+cx;
   moveto(78,((getmaxy-100)div 2)-4); outtext(cx);
-
-
-
-
-  generovanie(4);
-
-
 end;
 
-procedure predpis();
+procedure predpis();  //input predpisu
 begin
   input:='';
+  setfillstyle(1,black);
+  bar(250,getmaxy-110,getmaxx-220,getmaxy-90);
   moveto(250,getmaxy-110);
   SetTextStyle (0,0,2);setcolor(green);
   repeat
       if keypressed then begin
         ch:=readkey;
-        if (((ord(ch)>=40)and(ord(ch)<=57))or (ord(ch)=88) or (ord(ch)=120)) and ((ch<>#13)or(ch<>#8)) then begin
+        if (((ord(ch)>=40)and(ord(ch)<=57))or (ord(ch)=88) or (ord(ch)=120)) and ((ch<>#13)or(ch<>#8)or(ch<>#27)) then begin
            outtext(ch);
            input:=input+ch;
         end;
         if (ch=#8) then begin
           input:='';
           setfillstyle(1,black);
-          bar(250,getmaxy-110,getmaxx-200,getmaxy-90);
+          bar(250,getmaxy-110,getmaxx-220,getmaxy-90);
           moveto(250,getmaxy-110);
         end;
-        if (ch=#13) and ((input[1]='*') or (input[1]='/') or (input[1]='.') or (input[1]=',')) then begin
-          input:='';
-          setfillstyle(1,black);
-          bar(250,getmaxy-110,getmaxx-200,getmaxy-90);
-          moveto(250,getmaxy-110);
+        if (ch=#13) and (length(input)>0)then begin
+          if ((input[1]='*') or (input[1]='/') or (input[1]='.') or (input[1]=',')) then begin
+             input:='';
+             setfillstyle(1,black);
+             bar(250,getmaxy-110,getmaxx-220,getmaxy-90);
+             moveto(250,getmaxy-110);
+          end;
         end;
+        if (ch=#27) then esc:=1;
       end;
-  until (ch=#13) and (length(input)>0);
-  if (input[1]<>'+') and (input[1]<>'-') then insert('+',input,1);
-  SetTextStyle (0,0,1);setcolor(white);
-  moveto(100,20);
-  outtext(input);
+  until ((ch=#13) and (length(input)>0)) or (esc=1);
+  if (esc=0) then begin
+    if (input[1]<>'+') and (input[1]<>'-') then insert('+',input,1);
+    if pos(',',input)<>0 then begin
+      repeat
+          input[pos(',',input)]:='.';
+      until pos(',',input)=0;
+    end;
+  end;
+  //SetTextStyle (0,0,1);setcolor(white);moveto(100,20);outtext(input);
 end;
 
-procedure hra();
+function sipky(sipky_x,sipky_y,sipky_sirka,sipky_vyska,skok,nn:integer; farba_sipky:word):integer; //sipky v menu
+var x,p:integer;
 begin
+  x:=1;
+  p:=0;
+  sipky_y:=sipky_y+(((getmaxy-250) div nn) div 2);
+  setfillstyle(1,darkgray);
+  bar(sipky_x-(sipky_sirka div 2)+7,sipky_y+4,sipky_x+(sipky_sirka div 2)+7,sipky_y+sipky_vyska+4);
+  setfillstyle(1,farba_sipky);
+  bar(sipky_x-(sipky_sirka div 2),sipky_y,sipky_x+(sipky_sirka div 2),sipky_y+sipky_vyska);
+  repeat
+     keypressed();
+     ch:=readkey;
+     if (lowercase(ch)='s') and (x<nn) then begin    //sipka dole
+       setfillstyle(1,lightgray);
+       bar(sipky_x-(sipky_sirka div 2)-1,(sipky_y+skok*p)-1,sipky_x+(sipky_sirka div 2)+8,(sipky_y+skok*p)+sipky_vyska+4);
+       p:=p+1;
+       x:=x+1;
+       setfillstyle(1,darkgray);
+       bar(sipky_x-(sipky_sirka div 2)+7,(sipky_y+skok*p)+4,sipky_x+(sipky_sirka div 2)+7,(sipky_y+skok*p)+sipky_vyska+4);
+       setfillstyle(1,farba_sipky);
+       bar(sipky_x-(sipky_sirka div 2),(sipky_y+skok*p),sipky_x+(sipky_sirka div 2),(sipky_y+skok*p)+sipky_vyska);
+     end
+     else if (lowercase(ch)='w') and (x>1) then begin     //sipka hore
+       setfillstyle(1,lightgray);
+       bar(sipky_x-(sipky_sirka div 2)-1,(sipky_y+skok*p)-1,sipky_x+(sipky_sirka div 2)+8,(sipky_y+skok*p)+sipky_vyska+4);
+       p:=p-1;
+       x:=x-1;
+       setfillstyle(1,darkgray);
+       bar(sipky_x-(sipky_sirka div 2)+7,(sipky_y+skok*p)+4,sipky_x+(sipky_sirka div 2)+7,(sipky_y+skok*p)+sipky_vyska+4);
+       setfillstyle(1,farba_sipky);
+       bar(sipky_x-(sipky_sirka div 2),(sipky_y+skok*p),sipky_x+(sipky_sirka div 2),(sipky_y+skok*p)+sipky_vyska);
+     end;
+  until ch=#13;
+  sipky:=x;
+end;
+
+procedure menu(moznosti:integer);  //menu rozhranie
+var
+  space_cele,space_moznost,vys,sir,a:integer;
+begin
+  box(getmaxx div 2,getmaxy div 2,getmaxx,getmaxy,20,lightgray,white,darkgray);
+
+  //nadpis
+  settextstyle(0,0,10);
+  setcolor(darkgray);
+  outtextxy((getmaxx div 2)-305,55,'Graphwar');
+  setcolor(green);
+  outtextxy((getmaxx div 2)-300,50,'Graphwar');
+  settextstyle(0,0,1);setcolor(white);
+
+
+  sir:=600;
+  space_cele:=getmaxy-250;
+  space_moznost:=space_cele div moznosti;
+  vys:=space_moznost;
+  box(getmaxx div 2,270+((space_cele-200) div 2),sir+40,getmaxy-210,20,lightgray,darkgray,white);
+  a:=0;
+  for i:=1 to moznosti do begin
+     box(getmaxx div 2,170+(vys div 2)+a,sir,vys,20,lightgray,white,darkgray);
+     a:=a+vys;
+  end;
+
+end;
+
+
+procedure hra();     //cela hra
+var
+  vy,koniec,t:integer;
+begin
+  koniec:=0;
+  repeat
+    menu(4);
+    vy:=((getmaxy-250) div 4);
+    settextstyle(0,0,7);setcolor(darkgray);
+    outtextxy((getmaxx div 2)-103,170+(vy div 2)-25,'Hrat');
+    outtextxy((getmaxx div 2)-213,170+(vy div 2)+vy-25,'Rebricek');
+    outtextxy((getmaxx div 2)-213,170+(vy div 2)+(2*vy)-25,'Napoveda');
+    outtextxy((getmaxx div 2)-158,170+(vy div 2)+(3*vy)-25,'Koniec');
+    setcolor(green);
+    outtextxy((getmaxx div 2)-100,(170+(vy div 2))-29,'Hrat');
+    outtextxy((getmaxx div 2)-210,(170+(vy div 2)+vy)-29,'Rebricek');
+    outtextxy((getmaxx div 2)-210,(170+(vy div 2)+(2*vy))-29,'Napoveda');
+    outtextxy((getmaxx div 2)-155,(170+(vy div 2)+(3*vy))-29,'Koniec');
+    case sipky(getmaxx div 2,205,100,7,((getmaxy-250) div 4),4,green) of
+    1:begin
+           menu(5);
+           esc:=0;
+           stavhry:=0;
+           vy:=((getmaxy-250) div 5);
+           settextstyle(0,0,7);setcolor(darkgray);
+           outtextxy((getmaxx div 2)-178,170+(vy div 2)-25,'Level 1');
+           outtextxy((getmaxx div 2)-178,170+(vy div 2)+vy-25,'Level 2');
+           outtextxy((getmaxx div 2)-178,170+(vy div 2)+(2*vy)-25,'Level 3');
+           outtextxy((getmaxx div 2)-178,170+(vy div 2)+(3*vy)-25,'Level 4');
+           outtextxy((getmaxx div 2)-103,170+(vy div 2)+(4*vy)-25,'Spat');
+           setcolor(green);
+           outtextxy((getmaxx div 2)-175,(170+(vy div 2))-29,'Level 1');
+           outtextxy((getmaxx div 2)-175,(170+(vy div 2)+vy)-29,'Level 2');
+           outtextxy((getmaxx div 2)-175,(170+(vy div 2)+(2*vy))-29,'Level 3');
+           outtextxy((getmaxx div 2)-175,(170+(vy div 2)+(3*vy))-29,'Level 4');
+           outtextxy((getmaxx div 2)-100,(170+(vy div 2)+(4*vy))-29,'Spat');
+           case sipky(getmaxx div 2,202,100,7,((getmaxy-250) div 5),5,green) of
+           1:begin
+                  pole();t:=0;
+                  generovanie(1);
+                  repeat
+                    t:=t+1;
+                    if t<>1 then begin
+                       predpis();
+                       if esc<>1 then begin
+                          vykreslenie(hraci[1].x,hraci[1].y,black,input_old);
+                          vykreslenie(hraci[1].x,hraci[1].y,magenta,input);
+                          input_old:=input;
+                       end;
+                    end
+                    else if t=1 then begin
+                       predpis();
+                       if esc<>1 then begin
+                          vykreslenie(hraci[1].x,hraci[1].y,magenta,input);
+                          input_old:=input;
+                       end;
+                    end;
+                  until (stavhry=1) or (esc=1);
+             end;
+           2:begin
+                  pole();t:=0;
+                  generovanie(2);
+                  repeat
+                    t:=t+1;
+                    if t<>1 then begin
+                       predpis();
+                       if esc<>1 then begin
+                          vykreslenie(hraci[1].x,hraci[1].y,black,input_old);
+                          vykreslenie(hraci[1].x,hraci[1].y,magenta,input);
+                          input_old:=input;
+                       end;
+                    end
+                    else if t=1 then begin
+                       predpis();
+                       if esc<>1 then begin
+                          vykreslenie(hraci[1].x,hraci[1].y,magenta,input);
+                          input_old:=input;
+                       end;
+                    end;
+                  until (stavhry=1) or (esc=1);
+             end;
+           3:begin
+                  pole();t:=0;
+                  generovanie(3);
+                  repeat
+                    t:=t+1;
+                    if t<>1 then begin
+                       predpis();
+                       if esc<>1 then begin
+                          vykreslenie(hraci[1].x,hraci[1].y,black,input_old);
+                          vykreslenie(hraci[1].x,hraci[1].y,magenta,input);
+                          input_old:=input;
+                       end;
+                    end
+                    else if t=1 then begin
+                       predpis();
+                       if esc<>1 then begin
+                          vykreslenie(hraci[1].x,hraci[1].y,magenta,input);
+                          input_old:=input;
+                       end;
+                    end;
+                  until (stavhry=1) or (esc=1);
+             end;
+           4:begin
+                  pole();t:=0;
+                  generovanie(4);
+                  repeat
+                    t:=t+1;
+                    if t<>1 then begin
+                       predpis();
+                       if esc<>1 then begin
+                          vykreslenie(hraci[1].x,hraci[1].y,black,input_old);
+                          vykreslenie(hraci[1].x,hraci[1].y,magenta,input);
+                          input_old:=input;
+                       end;
+                    end
+                    else if t=1 then begin
+                       predpis();
+                       if esc<>1 then begin
+                          vykreslenie(hraci[1].x,hraci[1].y,magenta,input);
+                          input_old:=input;
+                       end;
+                    end;
+                  until (stavhry=1) or (esc=1);
+             end;
+           5:;
+           end;
+
+      end;
+    2:begin
+      end;
+    3:begin
+      end;
+    4:begin
+           menu(2);
+           vy:=((getmaxy-250) div 2);
+           settextstyle(0,0,7);setcolor(darkgray);
+           outtextxy((getmaxx div 2)-103,170+(vy div 2)-25,'Spat');
+           outtextxy((getmaxx div 2)-153,170+(vy div 2)+vy-25,'Koniec');
+           setcolor(green);
+           outtextxy((getmaxx div 2)-100,(170+(vy div 2))-29,'Spat');
+           outtextxy((getmaxx div 2)-150,(170+(vy div 2)+vy)-29,'Koniec');
+           case sipky(getmaxx div 2,202,100,7,((getmaxy-250) div 2),2,green) of
+           1:koniec:=0;
+           2:koniec:=1;
+           end;
+      end;
+    end;
+
+  until koniec=1;
 end;
 
 begin
   randomize;
   gd:=detect;
   initgraph(gd,gm,'C:\lazarus');
-  pole();
-  predpis();
-  vykreslenie(hraci[1].x,hraci[1].y);
-  str(hraci[1].x,la);
-  outtextxy(100,50,la);
+  //pole();
+  //predpis();
+  //vykreslenie(hraci[1].x,hraci[1].y);
+  hra();
 
 
-  readln();
   closegraph();
 end.
-
